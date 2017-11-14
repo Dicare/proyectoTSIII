8cfe9ddecae154ba7c31d5ce6dc7662c4b37ae9b
Leiazc <sauljesusarce@gmail.com>
2017-11-14 14:44:16 -0500

    interfaz
    Interfaz inicio y crear cuenta.

--- TSIIIProyectoServidor-Cliente/src/ts3/cliente/gui/win_principal.java
+++ TSIIIProyectoServidor-Cliente/src/ts3/cliente/gui/win_principal.java
@@ -7,14 +7,14 @@ package ts3.cliente.gui;
 
 /**
  *
- * @author alumno
+ * @author sjarc
  */
-public class FormularioLogin extends javax.swing.JFrame {
+public class win_principal extends javax.swing.JFrame {
 
     /**
-     * Creates new form FormularioLogin
+     * Creates new form FormularioPrincipal
      */
-    public FormularioLogin() {
+    public win_principal() {
         initComponents();
     }
 
@@ -33,11 +33,11 @@ public class FormularioLogin extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 755, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 411, Short.MAX_VALUE)
         );
 
         pack();
@@ -60,20 +60,21 @@ public class FormularioLogin extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FormularioLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(win_principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FormularioLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(win_principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FormularioLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(win_principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FormularioLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(win_principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FormularioLogin().setVisible(true);
+                new win_principal().setVisible(true);
             }
         });
     }
